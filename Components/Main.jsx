9076e05214d49d0a06173617f1d520cdee3528a7
CarloMart88 <carlo.martino1988@gmail.com>
2025-08-28 18:58:42 +0200

    feat: bonus

--- Components/Main.jsx
+++ Components/Main.jsx
@@ -7,45 +7,51 @@ const languages = [
     title: "HTML",
     description:
       "HTML (HyperText Markup Language) è il linguaggio standard per creare pagine e applicazioni web. Struttura il contenuto web e fornisce elementi di base come titoli, paragrafi e immagini.",
+    type: "btn-success",
   },
   {
     id: 2,
     title: "CSS",
     description:
       "CSS (Cascading Style Sheets) è un linguaggio di stile utilizzato per descrivere la presentazione di un documento scritto in HTML o XML. Controlla il layout, i colori, i caratteri e l'aspetto complessivo di una pagina web.",
+    type: "btn-success",
   },
   {
     id: 3,
     title: "JavaScript",
     description:
       "JavaScript è un linguaggio di programmazione dinamico utilizzato per lo sviluppo web, che consente contenuti interattivi come grafici animati, pulsanti cliccabili e altre funzionalità dinamiche sui siti web.",
+    type: "btn-success",
   },
   {
     id: 4,
     title: "Node.js",
     description:
       "Node.js è un ambiente di runtime JavaScript lato server che permette di utilizzare JavaScript anche sul backend, consentendo agli sviluppatori di usare lo stesso linguaggio sia sul client che sul server.",
+    type: "btn-success",
   },
   {
     id: 5,
     title: "Express",
     description:
       "Express è un framework per applicazioni web minimalista e veloce per Node.js. Fornisce strumenti robusti per creare applicazioni web e API e semplifica la gestione delle rotte e delle richieste.",
+    type: "btn-success",
   },
   {
     id: 6,
     title: "ReactJS",
     description:
       "ReactJS è una libreria JavaScript per costruire interfacce utente, in particolare applicazioni a pagina singola. Consente agli sviluppatori di creare componenti UI riutilizzabili e di gestire efficacemente lo stato dell'applicazione.",
+    type: "btn-success",
   },
 ];
 
 // creo una base HTML per vedere i bottoni
 function Main() {
-  const [isOpen, setisOpen] = useState(null);
+  const [isOpen, setIsOpen] = useState(null);
 
-  const toggleDescription = (id) => {
-    setisOpen((current) => (current === id ? null : id));
+  const toogleOpen = (id) => {
+    setIsOpen((current) => (current === id ? null : id));
   };
 
   return (
@@ -64,7 +70,8 @@ function Main() {
                 <Button
                   key={id}
                   language={language}
-                  onClick={() => toggleDescription(id)}
+                  isActive={isOpen === id}
+                  onClick={() => toogleOpen(id)}
                 />
                 {isOpen === id && <p>{description}</p>}
               </>
--- Components/button.jsx
+++ Components/button.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
-function Button({ language, onClick }) {
-  const { id, title, description } = language;
+function Button({ language, onClick, isActive }) {
+  const { id, title, description, type } = language;
   return (
     <div className="col-2 p-2 g-1">
-      <button className="btn btn-primary" onClick={onClick}>
+      <button
+        className={`btn ${isActive ? "btn-success" : "btn-primary"}`}
+        onClick={onClick}
+      >
         {title}
       </button>
     </div>
